23e9fc1d44e46779eb47b88bf6787db0368b8b43
SpiderManAPV <spidermanapv@gmail.com>
2016-04-08 16:53:42 -0400

    Adds imports and imports the poly text

--- src/PolynomialCalculator.java
+++ src/PolynomialCalculator.java
@@ -3,9 +3,20 @@
 // Project 3
 // Polynomial Calculator
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class PolynomialCalculator {
 	public static void main(String[] args) {
 		String file = args[0];
 		System.out.println(file);
+		try {
+			Scanner polyFile = new Scanner(new File(args[0]));
+		} catch (FileNotFoundException e) {
+			System.out.println("The file is not found.");
+			System.out.println("Please enter a valid file name and try again.");
+		}
+
 	}
 }
