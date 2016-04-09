2fd6124656f0044a503c42b5a73638408c92a77d
SpiderManAPV <spidermanapv@gmail.com>
2016-04-08 20:13:35 -0400

    reads the file in using for loops

--- src/PolynomialCalculator.java
+++ src/PolynomialCalculator.java
@@ -14,12 +14,49 @@ public class PolynomialCalculator {
 	public static void main(String[] args) {
 		String file = args[0];
 		System.out.println(file);
+		poly1 = new LinkedList();
+		poly2 = new LinkedList();
 		try {
 			Scanner polyFile = new Scanner(new File(file));
 			while(polyFile.hasNext()) {
-				String line = polyFile.nextLine();
-				for(int i = 0; i < line.length(); i++) {
-
+				String line1;
+				String line2;
+				line1 = " "+polyFile.nextLine();
+				line2 = " "+polyFile.nextLine();
+				int count = 0;
+				int num = 0;
+				boolean isNeg = false;
+				for(int i = line1.length()-1; i>=0; i--) {
+					if(Character.isDigit(line1.charAt(i))) {
+						num*=10;
+						num+=Character.getNumericValue(line1.charAt(i));
+					} else if(line1.charAt(i) == '-') {
+						isNeg = true;
+					} else if(line1.charAt(i) == ' ') {
+						if(isNeg == true) {
+							num= num*-1;
+						}
+						poly1.add(count, num);
+						num = 0;
+						isNeg = false;
+					}
+					System.out.println(poly1);
+				}
+				for(int i = line2.length()-1; i>=0; i--) {
+					if(Character.isDigit(line2.charAt(i))) {
+						num*=10;
+						num+=Character.getNumericValue(line2.charAt(i));
+					} else if(line2.charAt(i) == '-') {
+						isNeg = true;
+					} else if(line2.charAt(i) == ' ') {
+						if(isNeg == true) {
+							num= num*-1;
+						}
+						poly2.add(count, num);
+						num = 0;
+						isNeg = false;
+					}
+					System.out.println(poly2);
 				}
 			}
 		} catch (FileNotFoundException e) {
